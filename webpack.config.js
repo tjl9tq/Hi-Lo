a2c1bda35a0b01b467a6f4a18933572363ef3dd2
Timothy Liu <31290322+tjl9tq@users.noreply.github.com>
2019-03-16 20:45:18 -0400

    minify stuff

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.tsx',
   module: {
     rules: [
@@ -32,8 +33,16 @@ module.exports = {
       template: './public/index.html',
       inject: true,
       minify: {
-        removeComments: false,
-        collapseWhitespace: false,
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
       },
     }),
   ]
